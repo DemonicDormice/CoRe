f4183a9f791c7d4868b7e97ddfe2b5e966e25f74
burnii <jan.barocka@web.de>
2017-11-11 15:34:30 +0100

    Started GUI for Login and Registration

---
+++ CoRe/Assets/Scripts/Data/User.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class User {
+	private static User instance;
+
+	public string username;
+	public string email;
+	public string password;
+
+	protected User(){}
+
+	public static User getInstance(){
+		if (instance == null) {
+			instance = new User ();
+		} 
+
+		return instance;
+	}
+
+
+}
