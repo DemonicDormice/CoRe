a485abc8ed46377b8d172a35e69ff2cf9f74dfc6
Socar <sebastian.grosshans@hotmail.de>
2017-12-13 22:18:04 +0100

    Hab weiter gecancled
    Zeug halt...

---
+++ CoRe/Assets/Scripts/WorldRealmEditorScripts/CreationMenu.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//The goal of the creation menu is, that at one time you can open the creation menu, type in a world name
+//hit "enter" (or better the create button) and whole world is created, uploaded, and will automatically
+//start - all in 30 seconds work.
+
+
+public class CreationMenu : MonoBehaviour {
+
+	public InputField playerExpectation;
+	public InputField worldName;
+	public InputField startCalendarDate;
+	public InputField endCalendarDate;
+	public InputField randomEditorSeed;
+
+	public GameObject CanvasWorldMenuI;
+	public GameObject PanelRandomWorldQuestionI;
+	public GameObject PanelResetQuestion;
+	public GameObject PanelLoadDatabase;
+	public GameObject PanelLoadFile;
+
+	public GameObject CanvasWorldMenuII;
+	public GameObject PanelRandomWorldQuestionII;
+	public GameObject PanelTimeZonePopUp;
+
+	public GameObject CanvasClimateMenu;
+	public GameObject PanelRandomClimateQuestion;
+
+	public GameObject CanvasNPCMenu;
+	public GameObject PanelRandomNPCQuestion;
+
+
+	// Use this for initialization
+	void Start () {
+//playerExpectation = 200;
+
+		CanvasWorldMenuI.GetComponent<Canvas> ().enabled = true;
+		PanelRandomWorldQuestionI.SetActive(false);
+		PanelResetQuestion.SetActive(false); 
+		PanelLoadDatabase.SetActive(false);
+		PanelLoadFile.SetActive(false);
+
+		CanvasWorldMenuII.GetComponent<Canvas> ().enabled = false;
+		PanelRandomWorldQuestionII.SetActive(false);
+		PanelTimeZonePopUp.SetActive(false);
+
+		CanvasClimateMenu.GetComponent<Canvas> ().enabled = false;
+		PanelRandomClimateQuestion.SetActive(false);
+	
+		CanvasNPCMenu.GetComponent<Canvas> ().enabled = false;
+		PanelRandomNPCQuestion.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+
+	public void openWorldFirstCanvas () {
+		CanvasWorldMenuI.GetComponent<Canvas> ().enabled = true;
+		CanvasWorldMenuII.GetComponent<Canvas> ().enabled = false;
+		CanvasClimateMenu.GetComponent<Canvas> ().enabled = false;
+		CanvasNPCMenu.GetComponent<Canvas> ().enabled = false;
+	}
+
+	public void openWorldSecondCanvas () {
+		CanvasWorldMenuI.GetComponent<Canvas> ().enabled = false;
+		CanvasWorldMenuII.GetComponent<Canvas> ().enabled = true;
+		CanvasClimateMenu.GetComponent<Canvas> ().enabled = false;
+		CanvasNPCMenu.GetComponent<Canvas> ().enabled = false;
+	}
+
+	public void openClimateCanvas () {
+		CanvasWorldMenuI.GetComponent<Canvas> ().enabled = false;
+		CanvasWorldMenuII.GetComponent<Canvas> ().enabled = false;
+		CanvasClimateMenu.GetComponent<Canvas> ().enabled = true;
+		CanvasNPCMenu.GetComponent<Canvas> ().enabled = false;
+	}
+
+	public void openNPCCanvas () {
+		CanvasWorldMenuI.GetComponent<Canvas> ().enabled = false;
+		CanvasWorldMenuII.GetComponent<Canvas> ().enabled = false;
+		CanvasClimateMenu.GetComponent<Canvas> ().enabled = false;
+		CanvasNPCMenu.GetComponent<Canvas> ().enabled = true;
+	}
+
+	public void openRandWorldQuestI () {
+		PanelRandomWorldQuestionI.SetActive(true);
+	}
+
+	public void openRandWorldQuestII () {
+		PanelRandomWorldQuestionI.SetActive(true);
+	}
+
+	public void openResetQuestion () {
+		PanelResetQuestion.SetActive(true);
+	}
+
+	public void openLoadDatabase () {
+		PanelLoadDatabase.SetActive(true);
+	}
+
+	public void openLoadFile () {
+		PanelLoadFile.SetActive(true);
+	}
+
+	public void openTimeZone () {
+		PanelTimeZonePopUp.SetActive(true);
+	}
+
+	public void openRandClimateQuest () {
+		PanelRandomClimateQuestion.SetActive(true);
+	}
+
+	public void openRandNPCQuest () {
+		PanelRandomNPCQuestion.SetActive(true);
+	}
+
+	public void closeAllPanels ()
+	{
+		//Well, this function just closes all open panels. So it's the close/no button for all panels.
+		PanelRandomWorldQuestionI.SetActive(false);
+		PanelRandomWorldQuestionI.SetActive(false);
+		PanelResetQuestion.SetActive(false);
+		PanelLoadDatabase.SetActive(false);
+		PanelLoadFile.SetActive(false);
+		PanelTimeZonePopUp.SetActive(false);
+		PanelRandomClimateQuestion.SetActive(false);
+		PanelRandomNPCQuestion.SetActive(false);
+	}
+
+	public void LoadWorldEditorScene(string WorldEditorScene) {
+		SceneManager.LoadScene (WorldEditorScene);
+	}
+
+	//In World Menu First: This function will on start of the creation menu scene put a random integer number into the "Random-Seed" textfield.
+	//World Menu Second will automatically be set to the same status. 
+	public void randomTheSeed () {
+
+	}
+
+	//In World Menu First: This function will random all the things "randomable" in the editor like what climate zones are active or if
+	//there are all military units playable in the human cultures, how many settlements there are in a realm etc. But it will leave 
+	//other things like standard start or end time of day on standard. It will NOT create the world. So the player still has to click
+	//on create...
+	//TODO: Pop-Up for Climate Menu and NPC Menu á la "Do you want to randomly set OTHERMENU?" and for World Menus "Do you want to randomly
+	//set climate and NPC?"
+	public void randomTheRest () {
+
+	}
+
+	//In World Menu First: This function will calculate and change the world size and realm size depending on expected players for
+	//the next round. This will just fill the textfields in World Menu Second with fitting numbers, the user still has to confirm
+	//or can change the numbers.
+	//World Menu Second will automatically be set to the same status. 
+	public void PlayerExpectation (int playerExpectation) {
+
+		//We will expect that a round over 120 days should start with two realms per player á 50 by 50 tiles.
+		playerExpectation = ((DataControllerEditor.realmSizeX * DataControllerEditor.realmSizeY) / 2500) * (DataControllerEditor.worldSizeX * DataControllerEditor.worldSizeY);
+	}
+
+	//In World Menu First: This function will calculate on starting the creation editor a Start Calendar Date - based on the real world
+	//calendar date PLUS five days. A Toggle "Start Now" will set the start date (when true) to the real world time and date the second 
+	//the user hits create.
+	//World Menu Second will automatically be set to the same status.
+	//Preview DayOfTheWeek will automatically be updated.
+	public void StartCalendarDate () {
+
+	}
+
+	//In World Menu First: This function will calculate on starting the creation editor a End Calendar Date - based on the on start
+	//calculated Start Calendar Date PLUS 120 Days (4 Months).
+	//World Menu Second will automatically be set to the same status.
+	//Preview DayOfTheWeek will automatically be updated.
+	public void EndCalendarDate () {
+
+	}
+
+	//In World Menu Second: This function für the run-time in days textfield will change the end date calendar textfield and the 
+	//end time of day textfield - and if the user changes things inside those fields the run-time textfield will be changed - 
+	//so that you can put a number of days in there and it will calculate the right calendar date for you. 
+	//Because run-time in days will be set to 120 by standard (4 months) the function will also on start of the CreationScene
+	//set a End Calendar Date based on the real world system time and the 120 days run-time.
+	public void RunTimeDays () {
+
+	}
+
+	//In NPC Menu: This little function will set NPC per Realm Quantity automaticalle to 1 every time, the "Only one NPC in the World?" 
+	//toggle is set true. And every time someone puts an number in "NPC per Realm Quantity" the 'highlander-toggle' will set on false
+	public void HighlanderNPC () {
+
+	}
+}
--- CoRe/Assets/Scripts/WorldRealmEditorScripts/DataControllerEditor.cs
+++ CoRe/Assets/Scripts/WorldRealmEditorScripts/DataControllerEditor.cs
@@ -1,20 +1,106 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
-public class DataControllerEditor {
+
+//In this script and the related GameObject "DataControllerEditor" are all the things stored, which get inputted into the 
+//creation menus and which the editor needs to create a world and the realms in the next szenes.
+
+
+public class DataControllerEditor : MonoBehaviour {
 	
 	public static DataControllerEditor instance;
 
 	private WorldData world;
 
+	//Variables we get from World Menu
+	public static string worldName;
+	public static int worldSizeX = 20;
+	public static int worldSizeY = 20;
+	public static int realmSizeX = 50;
+	public static int realmSizeY = 50;
+
+	public static int startCalendarDate;
+	public static int startTimeOfDay; //TODO: 19 Uhr bzw. 7:00 pm setzen
+	public static bool startOnCreation = false;
+	public static int endCalendarDate; //TODO: wird am Anfang automatisch berechnet aus Systemzeit und 183 Tage run-time.
+	public static int endTimeOfDay;
+
+	public static int randomCreationSeed = 1;
+
+
+	//Variables we get from Climate Menu
+	public static int portionColdClimate = 20;
+	public static int portionWarmClimate = 20;
+	public static int portionMediterraneanClimate = 20;
+	public static int portionDesertClimate = 20;
+	public static int portionTropicClimate = 20;
+
+	public static bool activeColdPlain = true;
+	public static bool activeColdHill = true;
+	public static bool activeColdMountain = true;
+	public static bool activeColdConiferous = true;
+	public static bool activeColdBarren = true;
+	public static bool activeWarmPlain = true;
+	public static bool activeWarmHill = true;
+	public static bool activeWarmMountain = true;
+	public static bool activeWarmConiferous = true;
+	public static bool activeWarmDeciduous = true;
+	public static bool activeWarmBarren = true;
+	public static bool activeMediterraneanPlain = true;
+	public static bool activeMediterraneanHill = true;
+	public static bool activeMediterraneanMountain = true;
+	public static bool activeMediterraneanDeciduous = true;
+	public static bool activeMediterraneanBarren = true;
+	public static bool activeDesertPlain = true;
+	public static bool activeDesertHill = true;
+	public static bool activeDesertMountain = true;
+	public static bool activeDesertDeciduous = true;
+	public static bool activeDesertHammada = true;
+	public static bool activeDesertSand = true;
+	public static bool activeTropicPlain = true;
+	public static bool activeTropicHill = true;
+	public static bool activeTropicMountain = true;
+	public static bool activeTropicDeciduous = true;
+	public static bool activeTropicBarren = true;
+	public static bool activeWater = true;
+
+	//Variables we get from NPC Menu
+	public static int villagesRealm = 10;
+	public static int castlesRealm = 3;
+	public static int citiesRealm = 2;
+	public static int villagesRandom = 0;
+	public static int castlesRandom = 0;
+	public static int citiesRandom = 0;
+
+	public static bool highlanderNPC = true; //All settlements and npc armies are controlled by one and only one AI-Player - ignoring cultures and realms etc.
+	public static int quantityNPC = 1; //If highlandernNPC is true, quantityNPC is automatically set to 1
+
+	public static bool activeCultureNordic = true;
+	public static bool activeCultureMedieval = true;
+	public static bool activeCultureAncient = true;
+	public static bool activeCultureAsian = true;
+	public static bool activeCultureAfrican = true;
+	public static bool activeCultureHorde = true;
+
+	public static bool activeUnitSpecial = true;
+	public static bool activeUnitElite = true;
+	public static bool activeUnitAntiUnborn = true;
+
+
 	void Awake(){
 		instance = this;
 	}
 
-	public void createWorld(string name, string endtime, int xsize, int ysize, float coldC, float warmC, float medC, float desertC, float tropicC){
+	void Start () {
+
+		GameObject.DontDestroyOnLoad (this.gameObject);
+	}
+
+	/* 	public void createWorld(string name, string endtime, int xsize, int ysize, float coldC, float warmC, float medC, float desertC, float tropicC){
 		world = new WorldData ();
 		world.name = name;
 		//Call Random World Creation...
 
-	}
+	} */
 }
